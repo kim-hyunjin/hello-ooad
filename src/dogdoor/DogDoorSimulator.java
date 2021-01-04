97862959029382bc9782f828fd6a741846fd587f
Kim HyunJin <hyunjin1612@into.guru>
2021-01-04 15:55:23 +0900

    유스케이스, 주경로와 대체경로

--- src/dogdoor/DogDoorSimulator.java
+++ src/dogdoor/DogDoorSimulator.java
@@ -8,11 +8,16 @@ public class DogDoorSimulator {
 		System.out.println("흰둥이가 밖으로 나가고 싶어 짖는다");
 		remote.pressButton();
 		System.out.println("흰둥이가 밖으로 나갔다");
-		remote.pressButton();
 		System.out.println("흰둥이가 볼일을 본다");
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+		}
+		System.out.println("흰둥이가 밖에서 들어오지 못함");
+		System.out.println("흰둥이가 짖기 시작한다.");
+		System.out.println("주인이 리모컨을 집는다.");
 		remote.pressButton();
 		System.out.println("흰둥이가 안으로 들어온다.");
-		remote.pressButton();
 	}
 
 }
