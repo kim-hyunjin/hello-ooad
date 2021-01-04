dcb0accacf701064aafb65fb4a4759d466fcfea3
Kim HyunJin <hyunjin1612@into.guru>
2021-01-04 16:02:06 +0900

    요구사항의 변화, 캡슐화

--- src/dogdoor/DogDoor.java
+++ src/dogdoor/DogDoor.java
@@ -1,5 +1,8 @@
 package dogdoor;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class DogDoor {
 
 	private boolean open;
@@ -11,6 +14,13 @@ public class DogDoor {
 	public void open() {
 		System.out.println("The dog door opens.");
 		this.open = true;
+		final Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			public void run() {
+				close();
+				timer.cancel();
+			}
+		}, 5000);
 	}
 	
 	public void close() {
--- src/dogdoor/DogDoorSimulator.java
+++ src/dogdoor/DogDoorSimulator.java
@@ -4,9 +4,9 @@ public class DogDoorSimulator {
 
 	public static void main(String[] args) {
 		DogDoor door = new DogDoor();
-		Remote remote = new Remote(door);
+		BarkRecognizer recognizer = new BarkRecognizer(door);
 		System.out.println("흰둥이가 밖으로 나가고 싶어 짖는다");
-		remote.pressButton();
+		recognizer.recognize("왈왈");
 		System.out.println("흰둥이가 밖으로 나갔다");
 		System.out.println("흰둥이가 볼일을 본다");
 		try {
@@ -15,8 +15,7 @@ public class DogDoorSimulator {
 		}
 		System.out.println("흰둥이가 밖에서 들어오지 못함");
 		System.out.println("흰둥이가 짖기 시작한다.");
-		System.out.println("주인이 리모컨을 집는다.");
-		remote.pressButton();
+		recognizer.recognize("왈왈");
 		System.out.println("흰둥이가 안으로 들어온다.");
 	}
 
